ee8e137c605ebfb322ef22252620e103707f8335
Piotr Fidkowski <piotr@fidkowski.com>
2017-07-23 16:19:45 -0700

    initial commit

---
+++ recipe_list.js
@@ -0,0 +1,43 @@
+var kRecipes = {
+  // Breakfast.
+  'nalesniki': 'Nalesniki',
+  // Breads.
+  'brazilian_cheese_bread': 'Brazilian Cheese Bread',
+  'bread_beer': 'Beer Bread',
+  'bread_potato': 'Potato Bread',
+  // Soups.
+  'barszcz_z_uszkami': 'Barszcz z Uszkami',
+  // Mains.
+  'chicken_marsala': 'Chicken Marsala',
+  'chicken_piccata': 'Chicken Piccata',
+  'chile_verde': 'Chile Verde',
+  // Desserts.
+  'banana_bread': 'Banana Bread',
+  'cake_carrot': 'Carrot Cake',
+  'cake_szarlotka_z_budyniem': 'Szarlotka z Budyniem',
+};
+
+// Groupings for index.
+var kIndex = {
+  'Breakfast': [
+    'nalesniki',
+  ],
+  'Bread': [
+    'brazilian_cheese_bread',
+    'bread_beer',
+    'bread_potato',
+  ],
+  'Soups': [
+    'barszcz_z_uszkami',
+  ],
+  'Mains': [
+    'chicken_marsala',
+    'chicken_piccata',
+    'chile_verde',
+  ],
+  'Desserts': [
+    'banana_bread',
+    'cake_carrot',
+    'cake_szarlotka_z_budyniem',
+  ],
+};
